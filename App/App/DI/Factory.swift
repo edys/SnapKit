4e84be90316016beba72218c0bda00f6c4819ab0
edys <>
2021-05-13 23:04:55 +1200

    DI factory classes

---
+++ App/App/DI/Factory.swift
@@ -0,0 +1,15 @@
+//
+//  Factory.Swift
+//  PresentationLayer
+//
+//  Created by Rhys Walden on 13/05/21.
+//
+
+import Foundation
+import FieryCrucible
+
+/// Di Factory for the app (not 100% if this will be neede as of yet)
+public class Factory: DependencyFactory
+{
+   
+}
